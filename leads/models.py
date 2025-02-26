15056146986a4cf7b724b11c024903c2e7ef31a6
Urban <silc.urban10@gmail.com>
2025-02-26 19:55:47 +0100

    first commit

---
+++ leads/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Lead(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0) 
+    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
